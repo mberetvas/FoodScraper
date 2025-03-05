1d62e0ffad2060c064cdf8d9989163e771168ce3
Maxime Beretvas <maximeberetvas@outlook.com>
2025-03-05 11:55:21 +0100

    Update .gitignore and add new TOML selectors; remove obsolete recipe files, and encapsulate the logic of main in a loop.

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,8 @@ struct Recipe {
     steps: Option<Vec<String>>,
     /// A link to an image of the prepared recipe.
     image_link: Option<String>,
+    /// The URL source of the recipe.
+    source_url: String,
 }
 
 #[derive(Debug)]
@@ -35,60 +37,71 @@ struct Selectors {
     image: String,
 }
 
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // For debugging purposes print the content of the toml file on the teminal
-    let selectors = read_toml_file::<Value>("selectors.toml")?;
-    println!("{:#?}", selectors);
+    loop {
+        // Ask the user for the URL
+        let input_url = loop {
+            println!("Please enter the recipe URL:");
+            let mut input_url = String::new();
+            io::stdin().read_line(&mut input_url)?;
+            let input_url = input_url.trim();
 
-    // Ask the user for the URL
-    let input_url = loop {
-        println!("Please enter the recipe URL:");
-        let mut input_url = String::new();
-        io::stdin().read_line(&mut input_url)?;
-        let input_url = input_url.trim();
+            match validate_url(input_url) {
+                Ok(valid_url) => println!("Valid URL: {}", valid_url),
+                Err(err) => println!("Error: {}", err),
+            }
 
-        match validate_url(input_url) {
-            Ok(valid_url) => println!("Valid URL: {}", valid_url),
-            Err(err) => println!("Error: {}", err),
+            if Url::parse(input_url).is_ok() && (input_url.contains("https://15gram.be/") || input_url.contains("https://dagelijksekost.vrt.be/")) {
+                break input_url.to_string();
+            } else {
+                println!("Invalid URL or URL does not contain a supported domain. Please try again.");
+            }
+        };
+
+        let html_body = reqwest::get(&input_url).await?.text().await?;
+        let document = Html::parse_document(&html_body);
+
+        // Parse the website name from the URL
+        let website_name = parse_website_name(&input_url).ok_or("Failed to parse website name from URL")?;
+
+        // Load selectors from the TOML file
+        let selectors = load_selectors("selectors.toml", &website_name)?;
+
+        // Get the recipe details
+        let recipe = Recipe {
+            title: get_recipe_title(&document, &selectors.title, false),
+            description: get_recipe_description(&document, &selectors.description, false),
+            ingredients: get_recipe_ingredients(&document, &selectors.ingredients, false),
+            steps: get_recipe_steps(&document, &selectors.steps, false),
+            image_link: get_recipe_image(&document, &selectors.image, false),
+            source_url: input_url.clone(),
+        };
+
+        // Create a valid file name from the title
+        let file_name = match &recipe.title {
+            Some(title) => format!("recipe_{}.json", title),
+            None => "recipe.json".to_string(),
+        };
+
+        // Serialize the recipe to JSON and write to a file
+        let json = serde_json::to_string_pretty(&recipe)?;
+        std::fs::create_dir_all("recipes")?;
+        let file_path = format!("recipes/{}", file_name);
+        let mut file = File::create(&file_path)?;
+        file.write_all(json.as_bytes())?;
+
+        println!("Recipe JSON file '{}' created successfully.", file_name);
+
+        // Ask the user if they want to continue
+        println!("Do you want to scrape another recipe? (yes/no):");
+        let mut continue_input = String::new();
+        io::stdin().read_line(&mut continue_input)?;
+        if continue_input.trim().to_lowercase() != "yes" {
+            break;
         }
-
-        if Url::parse(input_url).is_ok() && input_url.contains("https://15gram.be/") {
-            break input_url.to_string();
-        } else {
-            println!("Invalid URL or URL does not contain 'https://15gram.be/'. Please try again.");
-        }
-    };
-
-    let html_body = reqwest::get(&input_url).await?.text().await?;
-    let document = Html::parse_document(&html_body);
-
-    // Load selectors from the TOML file
-    let selectors = load_selectors("selectors.toml", "15gram")?;
-
-    // Get the recipe details
-    let recipe = Recipe {
-        title: get_recipe_title(&document, &selectors.title, false),
-        description: get_recipe_description(&document, &selectors.description, false),
-        ingredients: get_recipe_ingredients(&document, &selectors.ingredients, false),
-        steps: get_recipe_steps(&document, &selectors.steps, false),
-        image_link: get_recipe_image(&document, &selectors.image, false),
-    };
-
-    // Create a valid file name from the title
-    let file_name = match &recipe.title {
-        Some(title) => format!("recipe_{}.json", title),
-        None => "recipe.json".to_string(),
-    };
-
-    // Serialize the recipe to JSON and write to a file
-    let json = serde_json::to_string_pretty(&recipe)?;
-    std::fs::create_dir_all("recipes")?;
-    let file_path = format!("recipes/{}", file_name);
-    let mut file = File::create(&file_path)?;
-    file.write_all(json.as_bytes())?;
-
-    println!("Recipe JSON file '{}' created successfully.", file_name);
+    }
 
     Ok(())
 }
@@ -215,10 +228,7 @@ fn get_recipe_image(document: &Html, css_selector: &str, verbose: bool) -> Optio
     image_link
 }
 
-fn read_toml_file<T: serde::de::DeserializeOwned>(
-    file_path: &str
-) -> Result<T, Box<dyn std::error::Error>> {
-    let content = fs::read_to_string(file_path)?;
-    let data: T = toml::from_str(&content)?;
-    Ok(data)
+fn parse_website_name(url: &str) -> Option<String> {
+    let url = Url::parse(url).ok()?;
+    url.host_str()?.split('.').next().map(|s| s.to_string())
 }
